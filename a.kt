57396223245a8208bd63512886797cb1b22fcde0
dipuAppDev <151380538+dipuAppDev@users.noreply.github.com>
2023-11-20 13:38:26 +0600

    Update a.kt

--- a.kt
+++ a.kt
@@ -1,6 +1,81 @@
-fun main(){
-    val a = 10
-    val b = 12
-    val c= a+b
-    println(" a+b = ${c}")
+fun main() {
+
+//println(
+//    "Hello,my name is Rakibul"
+//)
+//    println(
+//        "Hello World!"
+//    )
+
+//    print("Hello!How are you?\nAre you well or not?\n")
+//    println("I am App Developer.")
+//    println(
+//        "Good morning.Have you eaten your breakfast?"
+//    )
+//    println(
+//        "Android App Development:\nUsing Kotlin"
+//    )
+//    println(
+//        "This is my first kotlin coding class."
+//    )
+//    println(
+//        "JDK : Java Development Kit"
+//    )
+//
+//val subject = "Kotlin"
+//    val profession = "Android App Development"
+//    val a = 10
+//    println(a)
+//    var b = 20
+//    println(b)
+//    b=40
+//    println(b)
+//    var myHobbie = "Football"
+//    println(
+//        myHobbie
+//    )
+//    a=4
+
+    /*
+    Byte Datatype
+     */
+//    val num:Byte = 127
+//    val num2:Byte = -128
+//    println(num)
+//    println(num2)
+//
+//    val byteMaxValue = Byte.MAX_VALUE
+//    println(byteMaxValue)
+//    val byteMinValue = Byte.MIN_VALUE
+//    println(byteMinValue)
+//    val byteSize = Byte.SIZE_BYTES
+//    println(byteSize)
+//    val bitSize = Byte.SIZE_BITS
+//    println(bitSize)
+    /**
+     * Short Datatype
+     */
+//    val maxValue = Short.MAX_VALUE
+//    println(maxValue)
+//    val minValue = Short.MIN_VALUE
+//    println(minValue)
+//    val shortBytesValue = Short.SIZE_BYTES
+//    println(shortBytesValue)
+//    val shortBitsValue = Short.SIZE_BITS
+//    println(shortBitsValue)
+//    val value :Short = 22222
+    var a = Int.MAX_VALUE
+    var b = Int.MIN_VALUE
+    println("The maximum integer value is: ${a} and the minimum integer value is: ${b}")
+    val integerBitsNumber = Int.SIZE_BITS
+    val integerBytesNumber = Int.SIZE_BYTES
+    println(
+        " Integer Bytes Number is: ${integerBytesNumber} and Integer Bits Number is: ${integerBitsNumber}"
+    )
+val maxValue = Long.MAX_VALUE
+val minValue = Long.MIN_VALUE
+    println("Maximum long value : ${maxValue} and Minimum long value : ${minValue}")
+val longBytesValue = Long.SIZE_BYTES
+    val longBitsValue = Long.SIZE_BITS
+    println("Long Bytes value : ${longBytesValue} and Long Bits value : ${longBitsValue}")
 }
